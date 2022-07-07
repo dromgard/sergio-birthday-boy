6f3ff452e500f9bbc67ab06a33210cc9437f1b13
Sergei Korolev <dromgard@yandex.ru>
2022-07-07 11:50:09 +0300

    refactor: удалить лишние функции, добавить работу с массивом, разделить функции

--- script/index.js
+++ script/index.js
@@ -1,106 +1,104 @@
-const changeSerhio = document.querySelector('.sergio__common');
+// DATA
+// Массив поздравлений на разных языках.
+const congratulations = [
+  'Feliz cumpleaños, Serguéi!',
+  'Buon compleanno, Sergio!',
+  'Happy birthday, Sergei!',
+  'Alles Gute zum Geburtstag, Sergej!',
+  '生日快乐，塞尔吉!',
+  'З днем народження, Сергію!',
+  'Joyeux anniversaire, Sergueï!',
+  'С днем рождения, Сергей!'
+];
+
+// CONSTANTS
+// Выбираем элементы из DOM.
 const sergioCongratulation = document.querySelector('.congratulations__text');
+const serhioPhoto = document.querySelector('.sergio__common');
 const buttonAutoplay = document.querySelector('.controls__button_type_autoplay');
 const buttonAudioPlay = document.querySelector('.controls__button_type_audio');
 const audioFile = document.querySelector('.audio');
 
+// Задаем переменную для перебора массива поздравлений "congratulations".
+let congratulationsIndex = 0;
+
+// Задаем переменную для остановки слайдшоу.
 let autoplayIntervalID;
 
-let i = 1;
-const classNameDefault = 'sergio__common_random_';
-let className = classNameDefault + i;
+// ACTIONS
+// Возвращаем по очереди поздравление из массива.
+function returnCongratulation(array) {
+  if (congratulationsIndex < array.length) {
+    sergioCongratulation.textContent = array[congratulationsIndex];
+    congratulationsIndex += 1;
+  } else {
+    congratulationsIndex = 0;
+    sergioCongratulation.textContent = array[congratulationsIndex];
+    congratulationsIndex += 1;
+  }
+};
 
+// Запускаем возврат поздравления из массива с интервалом в 3 секунды.
+setInterval(() => returnCongratulation(congratulations), 3000);
+
+// Выдаем случайное число из переданного диапазона. 
 function randomInteger(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-function makeCongratulationSpanish() {
-    sergioCongratulation.textContent = 'Feliz cumpleaños, Serguéi!';
-}
+// Проверяем запущено ли слайдшоу. Да - останавливаем и запускаем ручной беребор.
+// Нет - Только ручной перебор.
+function checkSlideShowRunning() {
+  if (buttonAutoplay.classList.contains('playing')) {
+    stopSlideShow();
+    makeSerhioRandom();
+  } else {
+    makeSerhioRandom();
+  }
+};
 
-function makeCongratulationItalian() {
-    sergioCongratulation.textContent = 'Buon compleanno, Sergio!';
-}
-
-function makeCongratulationEnglish() {
-    sergioCongratulation.textContent = 'Happy birthday, Sergei!';
-}
-
-function makeCongratulationGerman() {
-    sergioCongratulation.textContent = 'Alles Gute zum Geburtstag, Sergej!';
-}
-
-function makeCongratulationChinise() {
-    sergioCongratulation.textContent = '生日快乐，塞尔吉!';
-}
-
-function makeCongratulationUkranian() {
-    sergioCongratulation.textContent = 'З днем народження, Сергію!';
-}
-
-function makeCongratulationFranch() {
-    sergioCongratulation.textContent = 'Joyeux anniversaire, Sergueï!';
-}
-
-function makeCongratulationRussian() {
-    sergioCongratulation.textContent = 'С днем рождения, Сергей!';
-}
-
-function makeCongratulationRandom() {
-    setTimeout(makeCongratulationSpanish, 3000);
-    setTimeout(makeCongratulationItalian, 6000);
-    setTimeout(makeCongratulationEnglish, 9000);
-    setTimeout(makeCongratulationGerman, 12000);
-    setTimeout(makeCongratulationChinise, 15000);
-    setTimeout(makeCongratulationUkranian, 18000);
-    setTimeout(makeCongratulationFranch, 21000);
-    setTimeout(makeCongratulationRussian, 24000);
-}
-
-makeCongratulationRandom();
-
-setInterval(makeCongratulationRandom, 24000);
-
+// Выдаем случайную фотографию, используя случайное число для формирования имени файла с фото.
 function makeSerhioRandom() {
-    clearInterval(autoplayIntervalID);
-    buttonAutoplay.classList.remove('playing');
-    buttonAutoplay.textContent = 'Autoplay';
-    makeSerhioRandomAutoplay();
-}
+  serhioPhoto.textContent = '';
+  serhioPhoto.style.backgroundImage = `url('images/sergio-${randomInteger(1, 87)}.png')`;
+};
 
-function makeSerhioRandomAutoplay() {
-    changeSerhio.textContent = '';
-    changeSerhio.classList.remove(className);
-    i = randomInteger(1, 87)
-    className = classNameDefault + i;
-    changeSerhio.classList.add(className);
-}
+// Останавливаем слайдшоу из случайных фотографий
+function stopSlideShow() {
+  clearInterval(autoplayIntervalID);
+  buttonAutoplay.classList.remove('playing');
+  buttonAutoplay.textContent = 'Autoplay';
+};
 
-function autoPlay() {
-    if (buttonAutoplay.classList.contains('playing') === false) {
-        makeSerhioRandomAutoplay();
-        var intervalID = setInterval(makeSerhioRandomAutoplay, 2000);
-        buttonAutoplay.classList.toggle('playing');
-        buttonAutoplay.textContent = 'Stop Autoplay';
-        autoplayIntervalID = intervalID;
-    } else {
-        clearInterval(autoplayIntervalID);
-        buttonAutoplay.classList.toggle('playing');
-        buttonAutoplay.textContent = 'Autoplay';
-        changeSerhio.textContent = 'Press me:)';
-    }
-}
+// Запускаем слайдшоу из случайных фотографий.
+function toggleSlideShow() {
+  if (buttonAutoplay.classList.contains('playing')) {
+    stopSlideShow();
+    serhioPhoto.textContent = 'Press me:)';
+  } else {
+    makeSerhioRandom();
+    const intervalID = setInterval(makeSerhioRandom, 2000);
+    buttonAutoplay.classList.add('playing');
+    buttonAutoplay.textContent = 'Stop Autoplay';
+    autoplayIntervalID = intervalID;
+  }
+};
 
+// Вставляем в DOM аудио трек, запускаем его, скрываем кнопку включения аудио.
 function playAudio() {
-    audioFile.innerHTML = `<audio controls loop src="audio/allegrova-happy-birthday.mp3" class="controls__audio" autoplay></audio>`;
-    audioFile.classList.remove('audio_hidden');
-    buttonAudioPlay.classList.add('controls__button_audio_hidden');
-}
+  audioFile.innerHTML = `<audio controls loop src="audio/allegrova-happy-birthday.mp3" class="controls__audio" autoplay></audio>`;
+  audioFile.classList.remove('audio_hidden');
+  buttonAudioPlay.classList.add('controls__button_audio_hidden');
+};
 
-changeSerhio.addEventListener('click', makeSerhioRandom);
+// LISTENERS
+// Кнопка ручного переключения фотографий.
+serhioPhoto.addEventListener('click', checkSlideShowRunning);
 
-buttonAutoplay.addEventListener('click', autoPlay);
+// Кнопка автоматического переключения фотографий.
+buttonAutoplay.addEventListener('click', toggleSlideShow);
 
+// Кнопка включения аудио.
 buttonAudioPlay.addEventListener('click', playAudio);
